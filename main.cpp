ac6fa0a148b7a6a5f161af818e80b6dc42546979
hiro1960 <hoge@exaple.com>
2022-08-26 10:46:26 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "mod1/hello.hpp"
+#include "mod2/bye.hpp"
+
+int main( ) {
+    hello( );
+    bye( );
+
+    return 0;
+}
---
+++ mod2/bye.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "bye.hpp"
+
+void bye( ) {
+    std::cout << "Bye!" << std::endl;
+}
